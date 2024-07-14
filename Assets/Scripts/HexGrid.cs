4f653bb5c9e694b274c309a5ae81e57a994c5480
Maria Shchurova <mary.shchurova@gmail.com>
2024-07-14 17:16:36 +0200

    "touching" and getting coordinates of cell in grid

--- Assets/Scripts/HexGrid.cs
+++ Assets/Scripts/HexGrid.cs
@@ -15,11 +15,13 @@ public class HexGrid : MonoBehaviour
 	Canvas gridCanvas;
 
 	HexMesh hexMesh;
+	MeshCollider meshCollider;
 
 	void Awake()
 	{
 		gridCanvas = GetComponentInChildren<Canvas>();
 		hexMesh = GetComponentInChildren<HexMesh>();
+		meshCollider = gameObject.AddComponent<MeshCollider>();
 
 		cells = new HexCell[height * width];
 
@@ -35,6 +37,32 @@ public class HexGrid : MonoBehaviour
     private void Start()
     {
 		hexMesh.Triangulate(cells);
+		meshCollider.sharedMesh = hexMesh.GetComponent<MeshFilter>().mesh;
+	}
+
+	void Update()
+	{
+		if (Input.GetMouseButton(0))
+		{
+			HandleInput();
+		}
+	}
+
+	void HandleInput()
+	{
+		Ray inputRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+		if (Physics.Raycast(inputRay, out hit))
+		{
+			TouchCell(hit.point);
+		}
+	}
+
+	void TouchCell(Vector3 position)
+	{
+		position = transform.InverseTransformPoint(position);
+		HexCoordinates coordinates = HexCoordinates.FromPosition(position);
+		Debug.Log("touched at " + coordinates.ToString());
 	}
 
 
