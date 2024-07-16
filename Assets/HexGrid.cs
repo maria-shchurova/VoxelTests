ba663fa177bb2cd200ff94cddaea6fca74e9511a
Maria Shchurova <mary.shchurova@gmail.com>
2024-07-16 21:04:55 +0200

    multi-layererd

--- Assets/HexGrid.cs
+++ Assets/HexGrid.cs
@@ -6,6 +6,7 @@ public class HexGrid : MonoBehaviour
 {
 	public int width = 6;
 	public int height = 6;
+	public int depth = 6;
 
 	public HexCell cellPrefab;
 
@@ -26,14 +27,17 @@ public class HexGrid : MonoBehaviour
 		hexMesh = GetComponentInChildren<HexMesh>();
 		meshCollider = gameObject.AddComponent<MeshCollider>();
 
-		cells = new HexCell[height * width];
+		cells = new HexCell[height * width * depth];
 		CellByCoordinates = new Dictionary<HexCoordinates, HexCell>();
 
 		for (int z = 0, i = 0; z < height; z++)
 		{
 			for (int x = 0; x < width; x++)
 			{
-				CreateCell(x, z, i++);
+				for (int y = 0; y < depth; y++)
+				{
+					CreateCell(x, z, y, i++);
+				}
 			}
 		}
 	}
@@ -73,18 +77,18 @@ public class HexGrid : MonoBehaviour
 	}
 
 
-	void CreateCell(int x, int z, int i)
+	void CreateCell(int x, int z,int y, int i)
 	{
 		Vector3 position;
 		position.x = (x + z * 0.5f - z / 2) * (HexMetrics.innerRadius * 2f);
-		position.y = 0f;
+		position.y = y * HexMetrics.height;
 		position.z = z * (HexMetrics.outerRadius * 1.5f);
 
 		HexCell cell = cells[i] = Instantiate<HexCell>(cellPrefab);
 		cell.transform.SetParent(transform, false);
 		cell.transform.localPosition = position;
-		cell.coordinates = HexCoordinates.FromOffsetCoordinates(x, z);
-		CellByCoordinates.Add(cell.coordinates, cell);
+		//cell.coordinates = HexCoordinates.FromOffsetCoordinates(x, z);
+		//CellByCoordinates.Add(cell.coordinates, cell);
 
 		//Text label = Instantiate<Text>(cellLabelPrefab);
 		//label.rectTransform.SetParent(gridCanvas.transform, false);
